7a173ea679835df7b7b1dadd089fa14a2213d35a
Tristan Lonsway <36086269+Kryptikz@users.noreply.github.com>
2018-02-15 15:29:35 -0500

    Update client.java

--- client.java
+++ client.java
@@ -1,22 +1,31 @@
-import java.net.*;
 import java.io.*;
-public class client {
-    private static Socket MyClient;
+import java.net.*;
+import cs1.*;
+public class MyClient {
     public static void connect(String ip, int port) {
+ 
+        Socket mainSocket = null;  
+        DataOutputStream os = null;
+        DataInputStream is = null;
+ 
         try {
-            MyClient = new Socket(ip, port);
+            mainSocket = new Socket(ip, port);
+            os = new DataOutputStream(mainSocket.getOutputStream());
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to: hostname");
+        }
+        System.out.println("What message do you want to send");
+        String message = Keyboard.readString();
+        try {
+            //DataOutputStream dOut = new DataOutputStream(mainSocket.getOutputStream());
+            while(true) {
+                message = Keyboard.readString();
+                os.writeUTF(message);
+                os.flush();
+            }
         }
         catch (IOException e) {
             System.out.println(e);
         }
-    }
-    public static void send(String message) {
-        PrintStream output;
-        try {
-            output = new PrintStream(MyClient.getOutputStream());
-        }
-        catch (IOException e) {
-            System.out.println(e);
-        }
-    }
-} 
+    }           
+}
